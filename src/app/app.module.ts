e2dcdb12b93aa7ee8ca120efec87b3e18574a914
Kumar2 <amkumar2@itron.com>
2018-09-20 12:49:41 +0530

    first time commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListUserComponent } from './list-user/list-user.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
+
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListUserComponent,
+    RegisterUserComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path:'', component:HomeComponent
+
+      },
+     
+      {
+        path:'test/register',component:RegisterUserComponent,
+        
+      },
+      {
+        path:'test/list', component:ListUserComponent
+      }
+    ]
+  )
   ],
   providers: [],
   bootstrap: [AppComponent]
